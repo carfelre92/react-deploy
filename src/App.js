a764d5b4a37467857c7a3722e2089ff892a8f933
Jin Kim <wlsgns92@hotmail.com>
2021-07-03 21:34:50 +1200

    deploy reat app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>my react app</h1>
+      <h2>hahaha</h2>
+      <img src={"https://via.placeholder.com/150"} alt="profile-image"/>
+    </div>
+  );
+}
+
+export default App;
